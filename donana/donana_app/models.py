d3a43953ef68d6ce388ad986bc9306d40bac491d
Mage <raphaelndub@gmail.com>
2025-05-24 22:01:51 -0300

    fiz a API

--- donana/donana_app/models.py
+++ donana/donana_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from PIL import Image
 
-# Create your models here.
+
+class Produtos(models.Model):
+    nome = models.CharField(max_length=50)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    descricao = models.TextField()
+    imagem = models.ImageField( upload_to='donana_app', blank=True, null=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.imagem:
+            img_path = self.imagem.path
+            img = Image.open(img_path)
+
+            if img.height > 800 or img.width > 800:
+                output_size = (800, 800)
+                img.thumbnail(output_size)
+
+               
--- donana/donana_app/views.py
+++ donana/donana_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Produtos
+from .serializers import ProdutosSerializers
 
-# Create your views here.
+class ProdutoViewSet (viewsets.ModelViewSet):
+    queryset = Produtos.objects.all()
+    serializer_class = ProdutosSerializers
